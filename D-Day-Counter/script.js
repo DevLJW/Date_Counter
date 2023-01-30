f600856d7043167784efb0ee30144851e72e283c
DevLJW <jaewookv123@gmail.com>
2023-01-30 21:01:19 +0900

    Interval add

--- D-Day-Counter/script.js
+++ D-Day-Counter/script.js
@@ -1,5 +1,7 @@
-const DayMessage = document.querySelector("#d-day-Message");
-
+const DayMessage = document.querySelector("#d-day-Message"); //D-day메세지
+const container = document.querySelector("#d-day-container"); //일시간분초
+DayMessage.innerHTML = "<h3>D -Day를 입력 해주세요.</h3>";
+container.style.display = "none"; //처음엔 안보이게 설정
 // DayMessage.textContent = "D -Day";
 const dateFormMarker = function () {
   const inputYear = document.querySelector("#target-year-input").value;
@@ -21,10 +23,16 @@ const counterMaker = function () {
   //만약 remaining이 0이면(차이시간) 타이머가 종료
   if (remaining <= 0) {
     DayMessage.innerHTML = "타이머가 종료 되었습니다.";
+    DayMessage.style.display = "flex";
+    container.style.display = "none"; //NAN 표시 지우기
+    return;
   } else if (isNaN(remaining)) {
     //만약 잘못된 날짜가 들어왔다면, 유효한 시간대가 아닙니다 출력
 
     DayMessage.innerHTML = "유효한 시간대가 아닙니다.";
+    DayMessage.style.display = "flex";
+    container.style.display = "none"; //NAN 표시 지우기
+    return;
   }
 
   //   const remainingDate = Math.floor(remaining / (60 * 60 * 24)); //날짜구하기 초 x 분 x 시간
@@ -39,19 +47,27 @@ const counterMaker = function () {
     remainingSec: Math.floor(remaining) % 60,
   };
 
-  const documentObj = {
-    days: document.getElementById("days"),
-    hours: document.getElementById("hours"),
-    min: document.getElementById("min"),
-    sec: document.getElementById("sec"),
-  };
-
+  const documentArr = ["days", "hours", "min", "sec"];
   const timeKeys = Object.keys(remainObj); //객체의 키만 가져오기 값X
-  const docKeys = Object.keys(documentObj);
-
-  for (let i = 0; i < timeKeys.length; i++) {
-    documentObj[docKeys[i]].textContent = remainObj[timeKeys[i]];
+  let i = 0;
+  // for of 배열에 많이 사용
+  for (let tag of documentArr) {
+    document.getElementById(tag).textContent = remainObj[timeKeys[i]];
+    i++;
   }
+
+  // const documentObj = {
+  //   days: document.getElementById("days"),
+  //   hours: document.getElementById("hours"),
+  //   min: document.getElementById("min"),
+  //   sec: document.getElementById("sec"),
+  // };
+
+  // let i = 0;
+  // for (key in documentObj) {
+  //   documentObj[key].textContent = remainObj[timeKeys[i]];
+  //   i++;
+  // }
 
   // documentObj["days"].textContent = remainObj["remainingDate"];
   // documentObj["hours"].textContent = remainObj["remainingHours"];
@@ -59,4 +75,12 @@ const counterMaker = function () {
   // documentObj["sec"].textContent = remainObj["remainingSec"];
 
   // console.log(remainingDate, remainingHours, remainingMin, remainingSec);
+};
+
+const starter = function () {
+  container.style.display = "flex";
+  DayMessage.style.display = "none";
+  counterMaker(); // setInterval같은경우 1초 뒤에 실행이 되어 자체적으로 강제 한번 실행 0 0 0표기 방지
+  const intervalId = setInterval(counterMaker, 1000); //1초마다 실행
+  //setInterval의 반환값은 interval의 고유의값이 반환 된다.
 };
